d2c16d43d51b5abe5a8b7c7df8b9e2500c966f81
Kr4v3n <fafachen37@gmail.com>
2020-01-19 10:19:50 +0100

    Setting App

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/chena/Documents/Projets divers/PYTHON DJANGO/knowledgeCity/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
