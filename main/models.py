88df6a449b3d1882bb2a99101dab633996432d4d
Kr4v3n <fafachen37@gmail.com>
2020-01-19 12:12:42 +0100

    Register App Main

--- main/models.py
+++ main/models.py
@@ -3,10 +3,15 @@ from django.db import models
 
 # Create your models here.
 
-class Main(models.Models):
+class Main(models.Model):
 
     name = models.TextField()
 
+    class Meta:
+        verbose_name = 'Principale'
+        verbose_name_plural = 'Principaux'
+
     def __str__(self):
         return self.name
+
 
