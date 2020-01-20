720f4f4fe5bcb924e028a5ca14c01155ee42de45
Kr4v3n <fafachen37@gmail.com>
2020-01-20 07:54:56 +0100

    Configure Main model

--- main/models.py
+++ main/models.py
@@ -4,10 +4,21 @@ from django.db import models
 # Create your models here.
 
 class Main(models.Model):
-    name = models.TextField()
+    name = models.CharField(max_length=50)
     about = models.TextField()
-    linkedin = models.TextField(default="-")
-    facebook = models.TextField(default="-")
-    twitter = models.TextField(default="-")
-    youtube = models.TextField(default="-")
-    github = models.TextField(default="-")
+    linkedin = models.CharField(max_length=100, default="-")
+    facebook = models.CharField(max_length=100, default="-")
+    twitter = models.CharField(max_length=100, default="-")
+    youtube = models.CharField(max_length=100, default="-")
+    github = models.CharField(max_length=100, default="-")
+    phone = models.CharField(max_length=100, default="-")
+    link_footer = models.CharField(max_length=100, default="-")
+
+    set_name = models.CharField(max_length=20, default="-")
+
+    def __str__(self):
+        return self.set_name + " | " + str(self.pk)
+
+    class Meta:
+        verbose_name = 'Général'
+        verbose_name_plural = 'Généraux'
