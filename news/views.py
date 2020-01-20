d397cef7c66d3b73e0780401391332a485a3c131
Kr4v3n <fafachen37@gmail.com>
2020-01-20 11:45:47 +0100

    Settings Up News Detail Page

--- news/views.py
+++ news/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import News
+from main.models import Main
 
 
 # Create your views here.
 
 
-def views():
-    pass
+def news_detail(request, pk):
+
+    site = Main.objects.get(pk=3)
+    news = News.objects.filter(pk=pk)
+
+    return render(request, 'front/news_detail.html', {'news': news, 'site': site})
