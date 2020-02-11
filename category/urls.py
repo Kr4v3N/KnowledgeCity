8b4d99bd4c6fd509c6e52430891e4c6570449e2c
Kr4v3n <fafachen37@gmail.com>
2020-02-11 12:04:38 +0100

    Add import data csv format and validation

--- category/urls.py
+++ category/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('panel/category/list', views.category_list, name='category_list'),
     path('panel/category/add', views.category_add, name='category_add'),
     path('export/category/csv', views.export_cat_csv, name='export_cat_csv'),
-
+    path('import/category/csv', views.import_cat_csv, name='import_cat_csv'),
 ]
--- category/views.py
+++ category/views.py
@@ -1,5 +1,4 @@
 import csv
-
 from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
@@ -9,7 +8,6 @@ from .models import Category
 # Create your views here.
 
 def category_list(request):
-
     # Login check start
     if not request.user.is_authenticated:
         return redirect('login')
@@ -21,7 +19,6 @@ def category_list(request):
 
 
 def category_add(request):
-
     # Login check start
     if not request.user.is_authenticated:
         return redirect('login')
@@ -40,7 +37,7 @@ def category_add(request):
             return redirect('category_add')
 
         else:
-            
+
             b = Category(name=name)
             b.save()
             messages.success(request, "La catégorie a bien été ajoutée")
@@ -54,6 +51,38 @@ def export_cat_csv(request):
     response['Content-Disposition'] = 'attachment; filename="category.csv'
 
     writer = csv.writer(response)
-    writer.writerow(['test1', 'test2', 'test3'])
-
+    writer.writerow(['Identifiant', 'Nom de la catégorie', "Nombre d'articles"])
+    for i in Category.objects.all():
+        writer.writerow([i.id, i.name, i.count])
     return response
+
+
+def import_cat_csv(request):
+    if request.method == 'POST':
+
+        csv_file = request.FILES['csv_file']
+
+        if not csv_file.name.endswith('.csv'):
+            messages.error(request, 'Mauvais format')
+            return redirect(category_list)
+
+        if csv_file.multiple_chunks():
+            messages.error(request, 'Le fichier est trop volumineux')
+            return redirect(category_list)
+
+        file_data = csv_file.read().decode("utf-8")
+        lines = file_data.split("\n")
+
+        for line in lines:
+
+            fields = line.split(",")
+
+            try:
+                if len(Category.objects.filter(Identifiant=fields[0])) == 0 and fields[0] != "Nom de la catégorie" \
+                        and fields[0] != "":
+                    b = Category(name=fields[0])
+                    b.save()
+            except:
+                print('finish')
+
+    return redirect('category_list')
